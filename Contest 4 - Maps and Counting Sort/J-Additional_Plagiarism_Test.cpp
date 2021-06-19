f49613c21e11fa9e8d5dbd8154ef360cdad9e50e
Georgy Trush <lkitjdp@gmail.com>
2021-06-19 10:40:35 +0300

    Completed Contest 4 :white_check_mark:

--- Contest 4 - Maps and Counting Sort/J-Additional_Plagiarism_Test.cpp
+++ Contest 4 - Maps and Counting Sort/J-Additional_Plagiarism_Test.cpp
@@ -1,86 +1,89 @@
-// Unsolved
+#include <unordered_map>
+#include <unordered_set>
+#include <iostream>
+#include <sstream>
+#include <cctype>
+#include <string>
 
-// #include <unordered_map>
-// #include <unordered_set>
-// #include <iostream>
-// #include <cctype>
-// #include <string>
-
-// using namespace std;
+using namespace std;
 
 
-// void MakeLowercase(string& potentialIdentifier) {
-//     for (char& symbol : potentialIdentifier) {
-//         symbol = tolower(symbol);
-//     }
-// }
+void MakeLowercase(string& potentialIdentifier) {
+    for (char& symbol : potentialIdentifier) {
+        symbol = tolower(symbol);
+    }
+}
 
-// bool DoesContainLetterOrUnderscore(const string& potentialIdentifier) {
-//     bool doesContainLetterOrUnderscore = false;
-//     for (const char& symbol : potentialIdentifier) {
-//         if (symbol == '_' || isalpha(symbol)) {
-//             doesContainLetterOrUnderscore = true;
-//             break;
-//         }
-//     }
-//     return doesContainLetterOrUnderscore;
-// }
+void LeaveOnlyLettersDigitsAndUnderscores(string& programLine) {
+    char symbol;
+    unsigned length = programLine.length();
+    for (unsigned i = 0; i < length; ++i) {
+        symbol = programLine[i];
+        if (symbol != '_' && isalnum(symbol) == false) programLine.replace(i, 1, " ");
+    }
+}
 
-// auto SplitIntoPotentialIdentifiers(const string& codeBlock) {
-//     char symbol;
-//     multiset<string> potentialIdentifiers;
-//     unsigned lastSplit = 0, length = codeBlock.length();
-//     for (unsigned i = 0; i < length; ++i) {
-//         symbol = codeBlock[i];
-//         if (symbol != '_' && isalnum(symbol) == false) {
-//             if (i > lastSplit) potentialIdentifiers.emplace(codeBlock.substr(lastSplit, i - lastSplit));
-//             lastSplit = i + 1;
-//         }
-//     }
-//     if (potentialIdentifiers.empty()) potentialIdentifiers.emplace(codeBlock);
-//     return potentialIdentifiers;
-// }
+bool IsCorrectIdentifier(const string& potentialIdentifier, const string& canDigitBeFirst) {
+    bool isDigit = true;
+    for (const char& symbol : potentialIdentifier) {
+        if (isdigit(symbol) == false) {
+            isDigit = false;
+            break;
+        }
+    }
+    bool isCorrectIdentifier = (isDigit || canDigitBeFirst == "no" && isdigit(potentialIdentifier.front()))?(false):(true);
+    return isCorrectIdentifier;
+}
 
-// int main() {
-//     unsigned numKeywords;
-//     cin >> numKeywords;
+int main() {
+    unsigned numKeywords;
+    cin >> numKeywords;
 
-//     string isCaseSensitive, canDigitBeFirst;
-//     cin >> isCaseSensitive >> canDigitBeFirst;
-    
-//     unordered_set<string> keywords;
-//     if (numKeywords > 0) {
-//         string keyword;
-//         for (unsigned i = 0; i < numKeywords; ++i) {
-//             cin >> keyword;
-//             if (isCaseSensitive == "no") MakeLowercase(keyword);
-//             keywords.emplace(keyword);
-//         }
-//     }
+    string isCaseSensitive, canDigitBeFirst;
+    cin >> isCaseSensitive >> canDigitBeFirst;
+    unordered_set<string> keywords;
+    if (numKeywords > 0) {
+        string keyword;
+        for (unsigned i = 0; i < numKeywords; ++i) {
+            cin >> keyword;
+            if (isCaseSensitive == "no") MakeLowercase(keyword);
+            keywords.emplace(keyword);
+        }
+    }
 
-//     string codeBlock;
-//     unordered_multiset<string> potentialIdentifiers;
-//     unordered_map<string, unsigned> identifierCounter;
-//     while (cin >> codeBlock) {
-//         potentialIdentifiers = SplitIntoPotentialIdentifiers(codeBlock);
-//         for (string potentialIdentifier : potentialIdentifiers) {
-//             if (isCaseSensitive == "no") MakeLowercase(potentialIdentifier);
-//             if (keywords.contains(potentialIdentifier) ||
-//                 isdigit(potentialIdentifier.front()) && (canDigitBeFirst == "no" ||
-//                 DoesContainLetterOrUnderscore(potentialIdentifier) == false)) continue;
-//             ++identifierCounter[potentialIdentifier];
-//         }
-//     }
+    unsigned newIdentifierIndex = 0;
+    string programLine, potentialIdentifier;
+    unordered_map<string, pair<unsigned, unsigned>> identifierCounter;
+    while (getline(cin, programLine)) {
+        LeaveOnlyLettersDigitsAndUnderscores(programLine);
+        stringstream clearedLine(programLine);
+        while (clearedLine >> potentialIdentifier) {
+            if (isCaseSensitive == "no") MakeLowercase(potentialIdentifier);
+            if (keywords.contains(potentialIdentifier) == false &&
+                IsCorrectIdentifier(potentialIdentifier, canDigitBeFirst)) {
+                if (identifierCounter.contains(potentialIdentifier)) {
+                    ++identifierCounter[potentialIdentifier].first;
+                } else {
+                    identifierCounter[potentialIdentifier].second = newIdentifierIndex;
+                    ++identifierCounter[potentialIdentifier].first;
+                    ++newIdentifierIndex;
+                }
+            }
+        }
+    }
 
-//     unsigned maxCount = 0;
-//     string mostFrequentIdentifier;
-//     for (const auto& [identifier, count] : identifierCounter) {
-//         if (count > maxCount) {
-//             maxCount = count;
-//             mostFrequentIdentifier = identifier;
-//         }
-//     }
-//     cout << mostFrequentIdentifier << endl;
+    string mostFrequentIdentifier;
+    unsigned maxCount = 0, minPosition = UINT32_MAX;
+    for (const auto& [identifier, countPosition] : identifierCounter) {
+        auto [count, position] = countPosition;
+        if (count > maxCount ||
+            count == maxCount && position < minPosition) {
+            maxCount = count;
+            minPosition = position;
+            mostFrequentIdentifier = identifier;
+        }
+    }
+    cout << mostFrequentIdentifier << endl;
 
-//     return EXIT_SUCCESS;
-// }
+    return EXIT_SUCCESS;
+}
