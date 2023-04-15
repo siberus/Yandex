cb6fd103edac4a92a7eea3896af8057d2828a7ac
Vladislav <mr.siberus@yandex.ru>
2023-04-15 20:52:05 +0700

    Add add() functon

--- 8. Trees/A-Tree-Own_solution.cpp
+++ 8. Trees/A-Tree-Own_solution.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <array>
 
 using namespace std;
 
@@ -9,14 +10,13 @@ private:
     struct Element
     {
         /* data */
-        int id = 0;
         int value = 0;
         int left = -1;
         int right = -1;
     };
 
     Element *element;
-    
+
     int num = 0;
     int level = 0;
     
@@ -25,27 +25,57 @@ public:
     {
         Element *newelement  = new Element [1];
         newelement->value = data;
-        newelement->id = 1;
         element = newelement;
         delete [] newelement;
         num = 1;
+        cout << "Объект создан. Корень дарева равен: " << data << ". Число лементов: " << num << endl;
     };
 
     ~Tree();
 
-    void add(int data)
+    
+    void add(int data, int index = 0)
     {
-        num = num++;
-        Element *newelement = new Element[num];
-        for (int  i = 0; i < num - 1; i++)
+        if(element[index].value == data)
+            return;
+        if (data < element[index].value)
         {
-            newelement[i] = element[i];
+            if (element[index].left == -1)
+            {
+                num = num++;
+                Element *newelement = new Element[num];
+                for (int  i = 0; i < num - 1; i++)
+                {
+                    newelement[i] = element[i];
+                }
+                newelement[index].left = num-1;
+                newelement[num - 1].value = data;  
+                element = newelement;
+                delete [] newelement;
+                cout << "Элемент создан: " << data << " Родитель: " << element[index].value <<  ". Число лементов: " << num << endl;
+            }
+            else add(data, element[index].left);
         }
-        newelement[num - 1].value = data;
-        newelement[num - 1].id =    
-        element = newelement;
-        delete [] newelement;
-        
+        else
+        {
+            if (element[index].right == -1)
+            {
+                num = num++;
+                Element *newelement = new Element[num];
+                for (int  i = 0; i < num - 1; i++)
+                {
+                    newelement[i] = element[i];
+                }
+                newelement[index].right = num-1;
+                newelement[num - 1].value = data;  
+                element = newelement;
+                delete [] newelement;
+                cout << "Элемент создан: " << data << " Родитель: " << element[index].value <<  ". Число лементов: " << num << endl;
+            }
+            else add(data, element[index].right);
+        }
+
+
     }
    
 
