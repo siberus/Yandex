2e3f65e0153a63e118c6be2edbe9ca40b48eafcb
Vladislav <mr.siberus@yandex.ru>
2023-04-22 20:01:04 +0700

    Add print

--- 2. Linear Search/I-inesweeper_own_solution.cpp
+++ 2. Linear Search/I-inesweeper_own_solution.cpp
@@ -44,7 +44,16 @@ int main()
         cin >> x >> y;
         addMine( &pole, x, y, m, n);
     }
-    
-
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            if (pole[i][j] == -1)
+            {
+                cout << "*";
+            }else            
+            cout << pole[i][j];
+        }
+    }
     return 0;
 }
