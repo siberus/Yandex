4d43473aad7aabc135bede6f5301b80d7fc4f292
Vladislav <mr.siberus@yandex.ru>
2023-04-22 19:56:49 +0700

    I own solution start

---
+++ 2. Linear Search/I-inesweeper_own_solution.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <array>
+
+using namespace std;
+
+vector<vector<int>> poleBuild(int m, int n){
+    vector<vector<int>> poleNull;
+    vector<int> line;
+    for(int i = 0; i <= m+1; i++){
+        for(int j = 0; j <= n+1; j++){
+            line.push_back(0);
+        }
+    poleNull.push_back(line);
+    }
+    return poleNull;
+}
+void addMine(vector<vector<int>> *pole, int x, int y, int m, int n){
+    for (int  i = 1; i <= m; i++)
+    {
+        for (int j = 1; i <= n; j++)
+        {
+            if (abs(y-i) == 1 || abs(x-j))
+            {
+                (*pole)[+i][j]++;
+            }
+            if (i == y && j == x)
+            {
+                (*pole)[i][j]=-1;
+            }
+        }
+    }
+    return;
+}
+
+int main()
+{
+    int m, n, k, x, y;
+    cin >> m >> n >> k;
+    vector<vector<int>> pole = poleBuild(m, n);
+    for (int i = 0; i < k; i++)
+    {
+        cin >> x >> y;
+        addMine( &pole, x, y, m, n);
+    }
+    
+
+    return 0;
+}
